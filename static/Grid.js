097c346bad7e2ae6eca8f7d457bf474eb8617dab
William Cole <ioetbc@gmail.com>
2021-08-22 16:27:15 +0100

    never touch

--- static/Grid.js
+++ static/Grid.js
@@ -79,11 +79,12 @@ function main() {
     }
 
     isTouch() {
-      return (
-        'ontouchstart' in window ||
-        navigator.maxTouchPoints > 0 ||
-        navigator.msMaxTouchPoints > 0
-      );
+      // return (
+      //   'ontouchstart' in window ||
+      //   navigator.maxTouchPoints > 0 ||
+      //   navigator.msMaxTouchPoints > 0
+      // );
+      false;
     }
 
     bind() {
